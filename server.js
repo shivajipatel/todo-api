d951dd0027c59b668311342fa6136f0403dec797
shivajipatel <shiva9907@gmail.com>
2018-07-09 00:49:44 +0530

    added post method to add todos

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 var express =   require("express");
+var bodyParser  =   require("body-parser");
 var app     =   express();
 var PORT    =   process.env.PORT || 3001;
-var todos   =   [
-                {
-                    'id':   1,
-                    'description':  'search collection merger',
-                    'completed':    false
-                },
-                {
-                    'id':   2,
-                    'description':  'mobile search revamp',
-                    'completed':    false
-                },
-                {
-                    'id':   3,
-                    'description':  'Filter changes',
-                    'completed':    true
-                }
-            ];
+var todos   =   [];
+var todoNextId  =   1;
+
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
     res.send('TO DO Api Root!');
@@ -40,6 +28,14 @@ app.get('/todos/:id',function(req,res){
     }else{
         res.status(404).send('todo id is not find');
     }
+});
+
+app.post('/todos',function(req,res){
+    var body    =   req.body;
+    body.id =   todoNextId;
+    todos.push(body);
+    todoNextId++;
+    res.json(body);
 });
 
 app.listen(PORT,function(){
